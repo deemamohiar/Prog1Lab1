b6b8851f5b63021ba0d9e4e1fa4ef772a186a140
deema <deema2002@live.ca>
2020-02-01 23:38:17 -0500

    created project

---
+++ MyAgeIs.java
@@ -0,0 +1,10 @@
+public class MyAgeIs {
+	
+	public static void main (String[]args){
+	int currentYear = 2020;
+	int yearOfBirth = 2002;
+	int age = currentYear - yearOfBirth;
+
+	System.out.println ("I will be" + " " + age + " this year!");
+	}
+}
