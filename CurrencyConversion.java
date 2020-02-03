f7d07d0429b5af646c66f145897dd001f0a8ec04
Deema <deema2002@live.ca>
2020-02-03 09:35:12 -0500

    added symbol sign

--- CurrencyConversion.java
+++ CurrencyConversion.java
@@ -3,8 +3,9 @@ public class CurrencyConversion {
 	double amountCAD = 65;
 	double amountUSD = amountCAD*0.76;
 	double amountEUR = amountCAD*0.68;
+	String euroSign = "\u20ac";
 
-System.out.println(amountCAD + " CAD$ = \n" + amountUSD + " USD$ \n" + amountEUR + " Euros");
+System.out.println(amountCAD + " CAD$ = \n" + amountUSD + " USD$ \n" + amountEUR + euroSign + " Euros");
 
 	}
 }
