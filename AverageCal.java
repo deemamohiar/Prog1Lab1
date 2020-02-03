4a6e10fe8212c7df086f7eb807472826c2c726af
Deema <deema2002@live.ca>
2020-02-03 09:53:41 -0500

    finished project

---
+++ AverageCal.java
@@ -0,0 +1,13 @@
+public class AverageCal {
+	public static void main(String[]args){
+	int one = 1;
+	int seven = 7;
+	int nine = 9;
+	int thirtyfour = 34;
+
+	int average = (one + seven + nine + thirtyfour)/4;
+
+	System.out.println("The average of 1,7,9 and 34 is: " + average + "!");
+
+	}
+}
